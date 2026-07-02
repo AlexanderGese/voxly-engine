6d9385b21142f6ba68232e7495c0fae9812c1383
AlexanderGese <alexander.gese07@gmail.com>
2026-07-02 11:54:43 +0200

    set up font

--- src/render/font.h
+++ src/render/font.h
@@ -1,3 +1,26 @@
 #ifndef RENDER_FONT_H
 #define RENDER_FONT_H
+
+// tiny fixed-width bitmap font. ascii 32..126. embedded as byte arrays.
+// 6x8 glyphs. good enough for hud text.
+
+#include "gl.h"
+
+typedef struct {
+    glid tex;
+    int  glyph_w;
+    int  glyph_h;
+    int  atlas_w;
+    int  atlas_h;
+} font;
+
+int  font_init(font *f);
+void font_destroy(font *f);
+void font_measure(const font *f, const char *s, int *out_w, int *out_h);
+
+// returns number of quads written into `out_verts`
+// each quad = 6 vertices of (px,py,u,v)
+int  font_build_text(const font *f, const char *s, int x, int y,
+                     float *out_verts, int max_verts);
+
 #endif
